21a9dde344d666c901554a6891d3685cd4fab69e
nemin <alddlv81@gmail.com>
2024-05-17 01:17:39 +0900

    refactor: private 빠진거 수정

--- src/main/java/com/ssafy/home/domain/service/AreaService.java
+++ src/main/java/com/ssafy/home/domain/service/AreaService.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Service
 public class AreaService {
 
-    AreaMapper areaMapper;
+    private final AreaMapper areaMapper;
 
     public AreaService(AreaMapper areaMapper) {
         this.areaMapper = areaMapper;
